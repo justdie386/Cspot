28230ecb5ceaf58537df2bf571e2b8acc6991f81
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2025-03-15 00:09:05 +0000

    Delete xmake.lua

--- xmake.lua
+++
@@ -1,9 +0,0 @@
-add_requires("vcpkg::libmicrohttpd", "libcurl", "cjson")
-
-
-target("Cspot")
-    add_files("src/*.c")
-    add_headerfiles("src/*.h")
-    add_packages("vcpkg::libmicrohttpd", "libcurl", "cjson")
-    add_links("Shell32")
-    --add_cflags("-fsanitize=address")
